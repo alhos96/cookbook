ec3e04312a5d866898935933b13f162006645ac6
Alija Hodzic <hodzic.alija5@gmail.com>
2021-12-15 15:43:42 +0100

    removed atlas uri

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5000;
 const app = express();
 const auth = require("./midleware/auth");
 
